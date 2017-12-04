71840762e2532ddfa7585f6b9d287854aadd4f5c
Anna Hotter <annajhotter@gmail.com>
2017-12-04 03:13:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function accessElementInArray(array, index){
   return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray {
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
   chocolateBars.shift()
   return chocolateBars
 }
