ee17b99a87363a420bc457ed4581bb1da3a05937
Anna Hotter <annajhotter@gmail.com>
2017-12-04 02:31:47 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var chocolateBars = [
+  'snickers' ,
+  'hundred grand' ,
+  'kitkat' ,
+  'skittles'
+]
+
+var nordicCapitals = [1]
+
+function addElementToBeginningOfArray(array, element) {
+  nordicCapitals.unshift("foo")
+}
