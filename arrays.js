0557eb89936a2c4415ac7c16e8099ea64f3388ed
Anna Hotter <annajhotter@gmail.com>
2017-12-04 03:28:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,7 +38,11 @@ function removeElementFromBeginningOfArray(chocolateBars) {
   return chocolateBars.slice(1)
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
+function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
   chocolateBars.pop()
   return chocolateBars
+}
+
+function removeElementFromEndOfArray(chocolateBars) {
+  return chocolateBars.slice(0, chocolateBars.length -1)
 }
