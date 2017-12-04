466325a684107ab929b8bc8a9dd61707b792806e
Anna Hotter <annajhotter@gmail.com>
2017-12-04 03:04:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,3 +24,7 @@ function destructivelyAddElementToEndOfArray(chocolateBars, element) {
   chocolateBars.push(element)
   return chocolateBars
 }
+
+function accessElementInArray(chocolateBars, index) {
+console.log(chocolateBars[0];)
+}
